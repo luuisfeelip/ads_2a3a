e12ffffb541877b1e63f114be05149c5ac6d704d
Luis Felipe <luuisfeelip@gmail.com>
2024-03-05 10:33:56 -0300

    Finalização da aula

---
+++ aula_04_javascript_pt1/exemplo06.js
@@ -0,0 +1,5 @@
+var nome = "Gonofredo"
+var idade = 40
+var salva = true
+var cidade;
+var tipo = typeof nome;
---
+++ aula_04_javascript_pt1/exemplo08.js
@@ -0,0 +1,4 @@
+var hora = 11
+if ((hora < 12) || (hora < 17)){
+    console.log("Você está na hora do rush")
+}
